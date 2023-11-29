073518865905069c329cf95ef928b3f33d83914f
mootre <65845604+mootre@users.noreply.github.com>
2023-11-29 14:19:03 +0700

    config redirect

--- src/lib/auth.tsx
+++ src/lib/auth.tsx
@@ -43,6 +43,13 @@ export const authOptions: NextAuthOptions = {
     signOut: '/auth',
   },
   callbacks: {
+    async redirect({ url, baseUrl }) {
+      // Allows relative callback URLs
+      if (url.startsWith("/")) return `${baseUrl}${url}`
+      // Allows callback URLs on the same origin
+      else if (new URL(url).origin === baseUrl) return url
+      return baseUrl
+    },
     async jwt({ token }) {
       return token;
     },
